e12d56833f6979f881b35d4d9506030181bf5083
Daniel Takayama <ydtak@umich.edu>
2017-11-01 11:32:24 -0400

    Starter files.

---
+++ background.js
@@ -0,0 +1,1 @@
+// TODO: manage tab content
---
+++ popup.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+    // TODO: read current tab content and store
+    document.getElementById("content").innerHTML = "Keccak";
+}
