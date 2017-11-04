4715fcf4a8f9122f5e94c73a4120792971ca4efe
ydtak <ydtak@umich.edu>
2017-11-04 16:43:30 -0400

    Fix bug not displaying tabs for all windows.

--- background.js
+++ background.js
@@ -7,7 +7,7 @@ var tabs = [];
  */
 var onGetWindows = function(windows) {
     for (var i = 0; i < windows.length; ++i) {
-        tabs = windows[i].tabs;
+        tabs = tabs.concat(windows[i].tabs);
     }
 }
 
