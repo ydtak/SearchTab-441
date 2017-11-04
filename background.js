7181bb181f5ff8535c750589b8bc5011675e6fe7
ydtak <ydtak@umich.edu>
2017-11-04 16:07:32 -0400

    Add script to retrieve open tabs. Add text input box.

--- background.js
+++ background.js
@@ -1,1 +1,18 @@
-// TODO: manage tab content
+/** Currently open tab data. */
+var tabs = [];
+
+/** 
+ * Callback method is triggered when all currently open chrome browser windows 
+ * are retrieved.
+ */
+var onGetWindows = function(windows) {
+    for (var i = 0; i < windows.length; ++i) {
+        tabs = windows[i].tabs;
+    }
+}
+
+function init() {
+    chrome.windows.getAll({populate: true}, onGetWindows);
+}
+
+init();
--- popup.js
+++ popup.js
@@ -1,4 +1,15 @@
+/** Reference to background page. */
+var bg = chrome.extension.getBackgroundPage();
+
+/** Generates HTML for search result item. */
+function createSearchResult(title) {
+    return `<p>` + title + `</p>`;
+}
+
 window.onload = function() {
-    // TODO: read current tab content and store
-    document.getElementById("content").innerHTML = "Keccak";
+    var search_contents = "";
+    for (var i = 0; i < bg.tabs.length; ++i) {
+        search_contents += createSearchResult(bg.tabs[i].title);
+    }
+    document.getElementById("content").innerHTML = search_contents;
 }
