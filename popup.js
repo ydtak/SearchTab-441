c4d5d077083cb7742dd271cc11df5bea304b9c92
ydtak <ydtak@umich.edu>
2017-11-04 16:26:30 -0400

    Add key listener for search box.

--- popup.js
+++ popup.js
@@ -6,10 +6,33 @@ function createSearchResult(title) {
     return `<p>` + title + `</p>`;
 }
 
-window.onload = function() {
+/** Renders new search results on popup. */
+function renderSearchResults(/* TODO */) {
     var search_contents = "";
     for (var i = 0; i < bg.tabs.length; ++i) {
-        search_contents += createSearchResult(bg.tabs[i].title);
+        var matches = true;
+        // TODO: implement matching
+        if (matches) {
+            search_contents += createSearchResult(bg.tabs[i].title);
+        }
     }
     document.getElementById("content").innerHTML = search_contents;
+}
+
+/** Callback method is triggered when user modifies search query in the search box. */
+var onSearchInputChanged = function(event) {
+    console.log(event); // TODO: remove debug message
+    var input_search_tab = document.getElementById("input_search_tab");
+    if (input_search_tab.value.trim() != "") {
+        renderSearchResults();
+    } else {
+        document.getElementById("content").innerHTML = "";
+    }
+
+}
+
+
+window.onload = function() {
+    var input_search_tab = document.getElementById("input_search_tab");
+    input_search_tab.addEventListener("input", onSearchInputChanged);
 }
