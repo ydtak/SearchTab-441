4a14327be6aad55414068b7fc18c96644119db42
ydtak <ydtak@umich.edu>
2017-11-04 16:36:47 -0400

    Add matching logic for searches.

--- popup.js
+++ popup.js
@@ -1,20 +1,31 @@
 /** Reference to background page. */
 var bg = chrome.extension.getBackgroundPage();
 
+/** Normalize string for string content comparisons. */
+function normalizeString(str) {
+    return str.trim().toUpperCase();
+}
+
+/** Returns true if str is a match for query. */
+function isMatch(str, query) {
+    return normalizeString(str).match(normalizeString(query));
+}
+
 /** Generates HTML for search result item. */
 function createSearchResult(title) {
     return `<p>` + title + `</p>`;
 }
 
 /** Renders new search results on popup. */
-function renderSearchResults(/* TODO */) {
+function renderSearchResults(search_query) {
     var search_contents = "";
-    for (var i = 0; i < bg.tabs.length; ++i) {
-        var matches = true;
-        // TODO: implement matching
-        if (matches) {
-            search_contents += createSearchResult(bg.tabs[i].title);
-        }
+    if (search_query.trim() != "") {
+        for (var i = 0; i < bg.tabs.length; ++i) {
+            var tab_title = bg.tabs[i].title;
+            if (isMatch(tab_title, search_query)) {
+                search_contents += createSearchResult(tab_title);
+            }
+        }    
     }
     document.getElementById("content").innerHTML = search_contents;
 }
@@ -23,12 +34,7 @@ function renderSearchResults(/* TODO */) {
 var onSearchInputChanged = function(event) {
     console.log(event); // TODO: remove debug message
     var input_search_tab = document.getElementById("input_search_tab");
-    if (input_search_tab.value.trim() != "") {
-        renderSearchResults();
-    } else {
-        document.getElementById("content").innerHTML = "";
-    }
-
+    renderSearchResults(input_search_tab.value);
 }
 
 
