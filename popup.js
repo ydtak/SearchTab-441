d7f1a038779bffb8e14fce8105ec3d004ac04bd8
ydtak <ydtak@umich.edu>
2017-11-05 15:15:29 -0500

    Implement switching tab functionality.

--- popup.js
+++ popup.js
@@ -11,28 +11,38 @@ function isMatch(str, query) {
     return normalizeString(str).match(normalizeString(query));
 }
 
-/** Generates HTML for search result item. */
-function createSearchResult(title) {
-    return `<p>` + title + `</p>`;
+/** Adds HTML for search result item and appends to div. */
+function addSearchResult(div, tab) {
+    var button = document.createElement("button");
+    button.innerHTML = tab.title;
+    button.onclick = function() {activateTab(tab.windowId, tab.id)};
+    div.appendChild(button);
+}
+
+/** Switches to tab identified by window_id and tab_id. */
+function activateTab(window_id, tab_id) {
+    chrome.windows.update(window_id, {focused: true});
+    chrome.tabs.update(tab_id, {active: true});
 }
 
 /** Renders new search results on popup. */
 function renderSearchResults(search_query) {
-    var search_contents = "";
+    // clear all old search results
+    var div = document.getElementById("content");
+    div.innerHTML = "";
+
     if (search_query.trim() != "") {
         for (var i = 0; i < bg.tabs.length; ++i) {
-            var tab_title = bg.tabs[i].title;
-            if (isMatch(tab_title, search_query)) {
-                search_contents += createSearchResult(tab_title);
+            var tab = bg.tabs[i];
+            if (isMatch(tab.title, search_query)) {
+                addSearchResult(div, tab);
             }
         }    
     }
-    document.getElementById("content").innerHTML = search_contents;
 }
 
 /** Callback method is triggered when user modifies search query in the search box. */
 var onSearchInputChanged = function(event) {
-    console.log(event); // TODO: remove debug message
     var input_search_tab = document.getElementById("input_search_tab");
     renderSearchResults(input_search_tab.value);
 }
