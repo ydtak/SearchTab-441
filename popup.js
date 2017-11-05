5986974b0f649a38f3c8da93b027ecd49fa78d4f
ydtak <ydtak@umich.edu>
2017-11-05 15:54:15 -0500

    Add grouping functionality.

--- popup.js
+++ popup.js
@@ -6,9 +6,10 @@ function normalizeString(str) {
     return str.trim().toUpperCase();
 }
 
-/** Returns true if str is a match for query. */
-function isMatch(str, query) {
-    return normalizeString(str).match(normalizeString(query));
+/** Returns true if tab is a match for query. */
+function isMatch(tab, query) {
+    return normalizeString(tab.title).match(normalizeString(query)) 
+        || normalizeString(tab.url).match(normalizeString(query));
 }
 
 /** Adds HTML for search result item and appends to div. */
@@ -25,6 +26,40 @@ function activateTab(window_id, tab_id) {
     chrome.tabs.update(tab_id, {active: true});
 }
 
+/** Onclick method for autogroup button. */
+function onClickAutogroup() {
+    var input_search_tab = document.getElementById("input_search_tab");
+    var div = document.getElementById("content");
+    div.innerHTML = "";
+    appendGroup(
+        div, 
+        "Stackoverflow", 
+        function(tab) {
+            return normalizeString(tab.url).match(normalizeString("stackoverflow"))
+        });
+    appendGroup(
+        div, 
+        "Misc", 
+        function(tab) {
+            return !normalizeString(tab.url).match(normalizeString("stackoverflow"))
+        });
+}
+
+function appendGroup(div, group_title, matcher) {
+    // create group title
+    var title_h3 = document.createElement("h3");
+    title_h3.innerHTML = group_title;
+    div.appendChild(title_h3);
+
+    // add tabs
+    for (var i = 0; i < bg.tabs.length; ++i) {
+        var tab = bg.tabs[i];
+        if (matcher(tab)) {
+            addSearchResult(div, tab);
+        }
+    }    
+}
+
 /** Renders new search results on popup. */
 function renderSearchResults(search_query) {
     // clear all old search results
@@ -34,7 +69,7 @@ function renderSearchResults(search_query) {
     if (search_query.trim() != "") {
         for (var i = 0; i < bg.tabs.length; ++i) {
             var tab = bg.tabs[i];
-            if (isMatch(tab.title, search_query)) {
+            if (isMatch(tab, search_query)) {
                 addSearchResult(div, tab);
             }
         }    
@@ -51,4 +86,9 @@ var onSearchInputChanged = function(event) {
 window.onload = function() {
     var input_search_tab = document.getElementById("input_search_tab");
     input_search_tab.addEventListener("input", onSearchInputChanged);
+
+    var button_autogroup = document.createElement("button");
+    button_autogroup.innerHTML = "Autogroup";
+    button_autogroup.onclick = function() {onClickAutogroup()};
+    document.getElementById("autogroup_div").appendChild(button_autogroup);
 }
